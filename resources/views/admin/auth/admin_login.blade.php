e4399918eda78dccacb6821410a3ec12a6572f8e
Jatin Bhatt <jatin.b.php@gmail.com>
2024-02-20 16:56:53 +0530

    change title

--- resources/views/admin/auth/admin_login.blade.php
+++ resources/views/admin/auth/admin_login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>{{ config('app.name', 'Laravel') }} | Log in</title>
+    <title>Log in | {{ config('app.name', 'SRS') }}</title>
     <meta name="_token" content="{!! csrf_token() !!}"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>{{ config('app.name', 'Shopping Cart') }} @if(isset($menu)) | {{$menu}} @endif</title>
+    <title>@if(isset($menu)) {{$menu}} | @endif {{ config('app.name', 'SRS') }}</title>
     <meta name="_token" content="{!! csrf_token() !!}"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="refresh" content = "28800; url={{ route('login') }}">
