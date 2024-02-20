33cd7b39c9991202e7dfb2022aef650dbaa7d2fa
Jatin Bhatt <jatin.b.php@gmail.com>
2024-02-20 14:20:18 +0530

    dashaboard box added

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\Supplier;
+use App\Models\Brand;
+use App\Models\Practice;
+use App\Models\StockOrder;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -12,6 +16,10 @@ class DashboardController extends Controller
     {
         $data['menu'] = "Dashboard";
         $data['users'] = User::where('role', '!=', 'admin')->count();
+        $data['suppliers'] = Supplier::count();
+        $data['brands'] = Brand::count();
+        $data['practices'] = Practice::count();
+        $data['stock_orders'] = StockOrder::count();
         return view('admin.dashboard', $data);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,7 +5,7 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-lg-3 col-6 mt-3">
-                        <div class="small-box bg-warning">
+                        <div class="small-box bg-info">
                             <div class="inner">
                                 <h3>{{$users}}</h3>
                                 <p>Total Users</p>
@@ -14,6 +14,42 @@
                                 <i class="fa fa-users"></i>
                             </div>
                             <a href="{{route('users.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-6 mt-3">
+                        <div class="small-box bg-success">
+                            <div class="inner">
+                                <h3>{{$suppliers}}</h3>
+                                <p>Total Suppliers</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fa fa-truck"></i>
+                            </div>
+                            <a href="{{route('suppliers.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-6 mt-3">
+                        <div class="small-box bg-warning">
+                            <div class="inner">
+                                <h3>{{$brands}}</h3>
+                                <p>Total Brands</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fa fa-boxes"></i>
+                            </div>
+                            <a href="{{route('brands.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-6 mt-3">
+                        <div class="small-box bg-danger">
+                            <div class="inner">
+                                <h3>{{$practices}}</h3>
+                                <p>Total Practices</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fa fa-folder"></i>
+                            </div>
+                            <a href="{{route('practices.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                         </div>
                     </div>
                 </div>
